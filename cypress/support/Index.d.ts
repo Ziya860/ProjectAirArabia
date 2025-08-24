67fc2e9e9766f62165ecffa0948576130da02fd5
Ziya860 <142209855+Ziya860@users.noreply.github.com>
2025-08-24 15:04:17 +0400

    book-ticket
    book-ticket

---
+++ cypress/support/Index.d.ts
@@ -0,0 +1,30 @@
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        /**
+         * Custom command to ... add your description here
+         * @example cy.clickOnMyJourneyInCandidateCabinet()
+         */
+        loginToMygovProdZ() : Chainable<null>;
+    }
+}
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        /**
+         * Custom command to ... add your description here
+         * @example cy.clickOnMyJourneyInCandidateCabinet()
+         */
+        loginMygovWithUI() : Chainable<null>;
+    }
+}
+
+
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        /**
+         * Custom command to ... add your description here
+         * @example cy.clickOnMyJourneyInCandidateCabinet()
+         */
+        findByPlaceholder(placeholderText: string) : Chainable<null>;
+    }
+}
+
