25cdee645bc9258a4f315311ce798a0869e2915d
Ziya860 <142209855+Ziya860@users.noreply.github.com>
2025-09-24 20:02:57 +0400

    newrunner
    test

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
 
-  // Reporter configuration
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
@@ -32,4 +31,30 @@ module.exports = defineConfig({
       },
     },
   },
+});
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Cypress Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: "cypress/reports/html",
+    overwrite: false,
+  },
+  e2e: {
+
+    setupNodeEvents(on, config) {
+
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  defaultCommandTimeout: 20000,
+  requestTimeout: 20000,
+  viewportWidth: 1920,
+  viewportHeight: 1080
 });
