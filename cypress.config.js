fb735a438a21dfd9a710b514bbda35af3b360925
Ziya860 <142209855+Ziya860@users.noreply.github.com>
2025-09-24 20:32:16 +0400

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,38 +1,4 @@
 const { defineConfig } = require("cypress");
-const path = require("path");
-
-module.exports = defineConfig({
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "My Cypress Test Report",
-    inlineAssets: true,
-    html: true,       // generate HTML later
-    json: false,
-    reportDir: "cypress/reports"
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      return config;
-    },
-    specPattern: "cypress/e2e/**/*.cy.js",
-
-    // Webpack aliases
-    webpack: {
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "cypress"),
-        },
-      },
-    },
-  },
-});
-const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
