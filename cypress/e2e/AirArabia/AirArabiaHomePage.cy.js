812be2e8690f0a4cfb59e458c34c55ffcb386924
Ziya860 <zabilov12@gmail.com>
2026-06-13 13:35:40 +0400

    update cypress tests

--- cypress/e2e/AirArabia/AirArabiaHomePage.cy.js
+++ cypress/e2e/AirArabia/AirArabiaHomePage.cy.js
@@ -1,5 +1,7 @@
 describe('Open AirArabia site', () => {
     it('should visit AirArabia homepage', () => {
         cy.visit('https://www.airarabia.com/')
+        cy.get('.pl-1').click();
     })
 })
+                        
--- cypress/e2e/AirArabia/BookTicket.cy.js
+++ cypress/e2e/AirArabia/BookTicket.cy.js
@@ -3,23 +3,24 @@ describe('Air Arabia book find', () => {
         cy.visit('https://www.airarabia.com/en');
 
         cy.get('#onetrust-accept-btn-handler').click();
-        cy.get('.swiper-slide-active > .banner-overlay > .row > .panel_content > .panel_inner > .show-for-medium').click()
         cy.get('input[name="flying-from"]').first().click().type('GYD');
         cy.get("#journey_type_rt").check({ force: true });
-        cy.wait(2000);
+        cy.wait(5000);
         cy.get('input[name="flying-to"]', { timeout: 10000 })
             .should('be.visible')
             .click({ force: true })
             .type('Phuket');
-        cy.contains('.airport-list .airportoption', 'Phuket').click({ force: true });
-        cy.wait(2000);
-        cy.get('input[name="deptdate"]')
-            .invoke('val', '10/12/2025')
+        
+    cy.get('.ff-dropdown-result > .ff-airport-list > .air-list-wrapper > li > .ff-airport-abr-outer > .ff-airport-abr')
+        .should('be.visible')
+        .click({ force: true });
+        cy.get('[name="departure-date"]')
+            .invoke('val', '10/12/2026')
             .trigger('change');
         cy.get('input[name="rtndate"]')
-            .invoke('val', '17/12/2025')
+            .invoke('val', '17/12/2026')
             .trigger('change');
-        cy.wait(2000);
+        cy.wait(5000);
 
         cy.get('select[name="search_currency"] option[value="USD"]')
             .should('exist')       // make sure the option exists
